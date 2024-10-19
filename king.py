34f100bc2b0dc5f6f4c641bb42b386a283c030be
RakeshAlgot-Hub <rakesh@aiyensi.com>
2024-10-19 17:18:26 +0530

    king rakesh

--- king.py
+++ king.py
@@ -2,6 +2,11 @@ print("king")
 print("queen")
 print("prince")
 print("princess")
+<<<<<<< HEAD
 print("soldier")
 print("soldiers Main")
 print("palace Main")
+print("soldier1")
+=======
+print("palace")
+>>>>>>> parent of e15aaae (Merge branch 'rakesh' of https://github.com/RakeshAlgot-Hub/fake-project into rakesh)
